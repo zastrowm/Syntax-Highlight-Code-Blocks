df8913e5b9f88e5e218d29c30c11c4f265d4fe8f
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2015-04-15 13:28:19 -0500

    Add specialized support for some MSDN sites

--- run-in-page.js
+++ run-in-page.js
@@ -1,8 +1,16 @@
+function isSiteMatched(hostname, path) {
+  return location.hostname == hostname
+         && location.pathname.startsWith(path)
+}
+
 /**
  * This function should only be invoked in the page where the context menu/browser button
  * was invoked and not in the background.js page
  */
 function highlightInCodeBlocks() {
+  
+  fixSimpleMsdnDocumentation();
+  
   var codes = document.querySelectorAll('pre code');
   
   for (var i = 0; i < codes.length; ++i) {
@@ -14,15 +22,49 @@ function highlightInCodeBlocks() {
 }
 
 /**
+ *  Fix code snippets on msdn.microsoft.com that only contain PRE and no CODE tags 
+ */
+function fixSimpleMsdnDocumentation() {
+  
+  // sometimes, documentation is only contained in a simple PRE block with no code
+  // element.  If that's the case, put the content into a PRE CODE so that later on
+  // the hightlight will work
+  if (isSiteMatched("msdn.microsoft.com", "/en-us/library/")) {
+    var pres = document.querySelectorAll('.codeSnippetContainerCodeContainer pre');
+  
+    for (var i = 0; i < pres.length; ++i) {
+      var pre = pres[i];
+      if (pre.children.length == []) {
+        // move any html in the pre into the code
+        var code = document.createElement("CODE");
+        code.innerHTML = pre.innerHTML;
+        pre.innerHTML = "";
+        pre.appendChild(code);
+        
+        hightlightCodeBlock(code, pre);
+      }
+    }
+  }
+  
+}
+
+/**
  * Highlight the given codeElement and the (optional) associated preElement
  * that contains it.
  */
 function hightlightCodeBlock(codeElement, preElement) {
   
   // ignore elements that have already been printed
-  if (preElement != null && preElement.classList.contains("prettyprint")) {
+  if (preElement != null && preElement.classList.contains("hljs")) {
     return;
   }
+  
+  // ignore elements that have already been printed
+  if (codeElement != null && codeElement.classList.contains("hljs")) {
+    return;
+  }
+  
+  fixMsdnMagazine(codeElement, preElement);
   
   var hasOnlyBRs = true;
 
@@ -45,6 +87,23 @@ function hightlightCodeBlock(codeElement, preElement) {
   }
 
   hljs.highlightBlock(codeElement);
+}
+
+function fixMsdnMagazine(codeElement, preElement) {
+  // I don't understand why MSDN puts code blocks in pre > code.xml blocks
+  // (and not in fixed-width at that), but if so, let hljs figure out how
+  // to format it instead
+  if (isSiteMatched("msdn.microsoft.com", "/en-us/magazine/")
+      && codeElement != null
+      && codeElement.className == "xml") {
+    
+    console.log("Fixing MSDN");
+    codeElement.className = "";
+    
+    if (preElement != null) {
+      preElement.style.fontFamily = "Consolas, monaco, monospace"
+    }
+  }
 }
 
 console.log("Highlighted Blocks");
